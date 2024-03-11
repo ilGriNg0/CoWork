abf413f7a21ecfc746c06afff32b25e4ee0ad8ff
whoz <arabchik122@icloud.com>
2024-03-11 17:35:39 +0300

    Добавлена карточка

--- AvaloniaApplication4/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApplication4/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace AvaloniaApplication4.ViewModels
+﻿using System;
+using Avalonia.Media.Imaging;
+using Avalonia.Skia.Helpers;
+
+namespace AvaloniaApplication4.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-#pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+        //public Bitmap? Image_bitmap_source { get; } = ImageLoad.Load("/Assets/prosto.jpg");
+
     }
 }
+//< Image Grid.ColumnSpan = "2"
+//Grid.RowSpan = "2"
+//Source = "{Binding Image_bitmap_source}"
+//Stretch = "Fill" ></ Image >
