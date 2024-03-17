cc7eaa324da9e18117d876f485d464601fc2d5e8
tst <arabc@LAPTOP-E95TEJK1>
2024-03-17 13:22:35 +0300

    Добавлен папка models, переделано в splitview

--- AvaloniaApplication4/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApplication4/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Reactive;
+using System.Runtime.CompilerServices;
 using Avalonia.Media.Imaging;
 using Avalonia.Skia.Helpers;
-
+using CommunityToolkit.Mvvm.ComponentModel;
+using ReactiveUI;
+using AvaloniaApplication4.Models;
 namespace AvaloniaApplication4.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public partial class MainWindowViewModel : ViewModelBase
     {
-        //public Bitmap? Image_bitmap_source { get; } = ImageLoad.Load("/Assets/prosto.jpg");
+        ////public Bitmap? Image_bitmap_source { get; } = ImageLoad.Load("/Assets/prosto.jpg");
+        
+        [ObservableProperty]
+        private ViewModelBase _page = new CardViewModel();
 
+       
+        [ObservableProperty] private CardModel? _cardOpen;
+      
+        public ObservableCollection<CardModel> cards { get; } = new()
+        {
+           new CardModel(typeof(CardViewModel))
+        };
     }
 }
 //< Image Grid.ColumnSpan = "2"
--- AvaloniaApplication4/ViewModels/ViewModelBase.cs
+++ AvaloniaApplication4/ViewModels/ViewModelBase.cs
@@ -1,8 +1,14 @@
-﻿using ReactiveUI;
+﻿
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace AvaloniaApplication4.ViewModels
 {
-    public class ViewModelBase : ReactiveObject
+    public class ViewModelBase  : ObservableObject
     {
+        
     }
 }
