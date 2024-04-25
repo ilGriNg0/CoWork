5d2769698d1a0f9ce77301fae582dbf17b18cdfc
Igor <igoranpoliakidse@gmail.com>
2024-04-26 01:38:43 +0300

    сделал окно выбора бизнеса или простого юзера, нихуя больше

--- AvaloniaApplication4/ViewModels/RegistrationPersonViewModel.cs
+++ AvaloniaApplication4/ViewModels/RegistrationPersonViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Npgsql;
 using System;
+using System.Diagnostics;
 using System.Diagnostics.Tracing;
 
 namespace AvaloniaApplication4.ViewModels
 {
     public partial class RegistrationPersonViewModel : ViewModelBase
     {
+        
+        
     }
 }
--- AvaloniaApplication4/Views/RegistrationPersonView.axaml.cs
+++ AvaloniaApplication4/Views/RegistrationPersonView.axaml.cs
@@ -1,4 +1,15 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Converters;
+using Avalonia.Data;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.Globalization;
 
 namespace AvaloniaApplication4.Views
 {
@@ -7,6 +18,28 @@ namespace AvaloniaApplication4.Views
         public RegistrationPersonView()
         {
             InitializeComponent();
+        }
+
+        public void Find_Click(object source, RoutedEventArgs args)
+        {
+            if (this.GetControl<Border>("woodcutter1").IsVisible)
+            {
+                this.GetControl<Border>("woodcutter1").IsVisible = false;
+                this.GetControl<Border>("woodcutter").IsVisible = true;
+            }
+            this.GetControl<Border>("spaceman1").IsVisible = true;
+            this.GetControl<Border>("spaceman").IsVisible = false;
+        }
+        public void Create_Click(object source, RoutedEventArgs args)
+        {
+            //this.GetControl<Image>("spaceman").IsVisible = false;
+            if (this.GetControl<Border>("spaceman1").IsVisible)
+            {
+                this.GetControl<Border>("spaceman1").IsVisible = false;
+                this.GetControl<Border>("spaceman").IsVisible = true;
+            }
+            this.GetControl<Border>("woodcutter").IsVisible = false;
+            this.GetControl<Border>("woodcutter1").IsVisible = true;
         }
     }
 }
